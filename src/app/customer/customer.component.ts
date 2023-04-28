215770a4ffff0b39208fe58f23a1626e4a9a3fd8
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-28 18:23:26 +0530

    first commit

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent {
+
+}
